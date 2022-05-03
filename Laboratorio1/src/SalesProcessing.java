865adfa5e2154e012f04182ae3ea934bf3f18afb
Jordi Bardales <jbardalesr@uni.pe>
2022-05-03 10:15:03 -0500

    Add SalesProcessing

--- Laboratorio1/src/SalesProcessing.java
+++ Laboratorio1/src/SalesProcessing.java
@@ -1,2 +1,13 @@
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.conf.Configuration;
+
 public class SalesProcessing {
+    public static void main(String[] args) {
+        
+    }
 }
