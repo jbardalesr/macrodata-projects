7c3715b608a991c0f2bf9377a171936a0489d514
Jordi Bardales <jbardalesr@uni.pe>
2022-05-03 21:27:27 -0500

    Total Price by PaymentType in each Country

---
+++ Laboratorio1/src/consulta1/TotalPriceByPayCountry.java
@@ -0,0 +1,50 @@
+package consulta1;
+
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.*;
+import org.apache.hadoop.mapred.*;
+
+/*
+Total Price by PaymentType in each Country
+United States-Amex      121700
+United States-Diners    45600
+United States-Mastercard        204350
+United States-Visa      366650
+*/
+
+public class TotalPriceByPayCountry {
+    public static void main(String[] args) {
+        JobClient my_client = new JobClient();
+        // Create a configuration object for the job
+        JobConf job_conf = new JobConf(TotalPriceByPayCountry.class);
+
+        // Set a name of the Job
+        job_conf.setJobName("SalePerCountry");
+
+        // Specify data type of output key and value
+        job_conf.setOutputKeyClass(Text.class);
+        job_conf.setOutputValueClass(IntWritable.class);
+
+        // Specify names of Mapper and Reducer Class
+        job_conf.setMapperClass(consulta1.TotalPriceByPayCountryMapper.class);
+        job_conf.setReducerClass(consulta1.TotalPriceByPayCountryReducer.class);
+
+        // Specify formats of the data type of Input and output
+        job_conf.setInputFormat(TextInputFormat.class);
+        job_conf.setOutputFormat(TextOutputFormat.class);
+
+        // Set input and output directories using command line arguments,
+        //arg[0] = name of input directory on HDFS, and arg[1] =  name of output directory to be created to store the output file.
+
+        FileInputFormat.setInputPaths(job_conf, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job_conf, new Path(args[1]));
+
+        my_client.setConf(job_conf);
+        try {
+            // Run the job
+            JobClient.runJob(job_conf);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ Laboratorio1/src/consulta2/SalesProcessing.java
@@ -0,0 +1,15 @@
+package pregunta2;
+
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.conf.Configuration;
+
+public class SalesProcessing {
+    public static void main(String[] args) {
+
+    }
+}
