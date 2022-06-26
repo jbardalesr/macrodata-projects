11758e2e5ed5418579affbb5091fadca3ff61c28
Jordi Bardales <jbardalesr@uni.pe>
2022-06-26 15:48:08 -0500

    consulta 3 y consulta 4 terminadas

--- Laboratorio3/src/consulta3.scala
+++ Laboratorio3/src/consulta3.scala
@@ -1,4 +1,23 @@
-val data = sc.textFile("data-limpia.csv")
-val mp = data.mapPartitionsWithIndex { (idx, iter) => if (idx == 0) iter.drop(1) else iter}
-val zones_stroke = mp.map(s =>  s.split(",")(7)+'-'+s.split(",")(11)).map(res_type => (res_type, 1)).reduceByKey(_ + _)
-zones_stroke.saveAsTextFile("consulta3.txt")
+// Consulta 3: ¿Donde se da la mayor cantidad de ataques, en zonas urbanas o rurales?
+
+///////////////// SCALA /////////////////
+val load_data = sc.textFile("data-limpia.csv")
+val data = load_data.mapPartitionsWithIndex { (idx, iter) => if (idx == 0) iter.drop(1) else iter }
+val stroke_by_zone = data.map(s => (s.split(",")(7) + '-' + s.split(",")(11), 1)).reduceByKey(_ + _)
+stroke_by_zone.saveAsTextFile("output3")
+
+///////////////// SPARK-SQL /////////////////
+val load_config = spark.read.format("csv").option("header", "true").option("inferSchema", "true") 
+val dfCSV = load_config.load("data-limpia.csv")
+// print
+dfCSV.show
+dfCSV.printSchema()
+// create table
+dfCSV.createOrReplaceTempView("dfTable")
+// sql query
+val countSql = spark.sql("""
+SELECT Residence_type, stroke, count(stroke) as count_stroke 
+FROM dfTable
+GROUP BY Residence_type, stroke
+""")
+countSql.show()
--- Laboratorio3/src/consulta4.scala
+++ Laboratorio3/src/consulta4.scala
@@ -1,7 +1,33 @@
-// ¿Edades promedio de los que sufrieron ataque según el tipo de empleo?
-val data = sc.textFile("data-numerica.csv")
-val mp = data.mapPartitionsWithIndex {(idx, iter) => if (idx == 0) iter.drop(1) else iter}
-val stroke_data = mp.filter(x => x.split(",")(11).equals("1"))
-val type_stroke = stroke_data.map(s =>  s.split(",")(6)+'-'+s.split(",")(2))
-val average_age = type_stroke.map(type_age => (type_age.split("-")(0), type_age.split("-")(1).toFloat)).reduceByKey(_ + _)
-zones_stroke.saveAsTextFile("output4")
+// Consulta 4: ¿Edades promedio de los que sufrieron ataque según el tipo de empleo?
+
+///////////////// SCALA /////////////////
+val load_data = sc.textFile("data-limpia.csv")
+val data = load_data.mapPartitionsWithIndex { (idx, iter) => if (idx == 0) iter.drop(1) else iter }
+// filtra unicamente aquellos con ataque stroke=1
+val stroke_data = data.filter(x => x.split(",")(11).equals("1"))
+// concatena work_type con las edades
+val type_stroke = stroke_data.map(s => s.split(",")(6) + ',' + s.split(",")(2))
+// separa work_type y edades para sumar las edades
+val mean_by_wtype = type_stroke.map(row => (row.split(",")(0), row.split(",")(1).toFloat))
+// suma y cuenta cada work_type
+val reduce_sum_count = mean_by_wtype.reduceByKey((x, y) => (x._1 + y._1, x._2 + y._2))
+// divide entre la suma y la cantidad de elementos en cada work_type
+val mean_age = reduce_sum_count.map(row => (row._1, row._2._1/row._2._2))
+mean_age.saveAsTextFile("output4")
+
+///////////////// SPARK-SQL /////////////////
+val load_config = spark.read.format("csv").option("header", "true").option("inferSchema", "true") 
+val dfCSV_Demo = load_config.load("stroke_demografico.csv")
+val dfCSV_Salud = load_config.load("stroke_salud.csv")
+
+// val dfJson = spark.read.json("stroke_demografico.json")
+dfCSV_Demo.createOrReplaceTempView("dfTableDemo")
+dfCSV_Salud.createOrReplaceTempView("dfTableSalud")
+
+val meanSql = spark.sql("""
+SELECT work_type, avg(age) as mean_age 
+FROM dfTableDemo JOIN dfTableSalud ON dfTableDemo.id = dfTableSalud.id 
+WHERE stroke == 1 
+GROUP BY work_type
+""")
+meanSql.show()
